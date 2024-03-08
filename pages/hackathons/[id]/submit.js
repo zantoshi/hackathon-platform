97450591c32df75d4d0e4dc2368c4a9d5968406f
Edgar Saravia <edgar.saravia@zebedee.io>
2024-03-08 09:54:21 -0600

    -do not allow an user to go to the page of submit project when it's not the time for submit it

--- pages/hackathons/[id]/submit.js
+++ pages/hackathons/[id]/submit.js
@@ -10,8 +10,9 @@ export default function SubmitProject() {
   const router = useRouter();
   const [id, setId] = useState("");
   const [hackathon, setHackathon] = useState([]);
-  const [project,setProject]= useState([])
-
+  const [project, setProject] = useState([]);
+  const currentDate = new Date();
+  const formattedCurrentDate = currentDate.toISOString().split("T")[0];
   useEffect(() => {
     if (router.isReady) {
       setId(router.query.id);
@@ -24,7 +25,7 @@ export default function SubmitProject() {
       try {
         if (id) {
           const response = await fetch(`/api/hackathons/${id}`, {
-            cache: 'no-store' ,
+            cache: "no-store",
             method: "GET",
             headers: { "Content-Type": "application/json" },
             next: { revalidate: 1 },
@@ -43,47 +44,60 @@ export default function SubmitProject() {
     fetchHackathon();
   }, [id]);
 
-  useEffect(()=>{
-    const fetchProject = async ()=>{
-      try{
-        if(id){
-          const response = await fetch(`/api/projects/${id}/projectUser`,{
-            method:"GET",
-            cache: 'no-store' ,
+  useEffect(() => {
+    const fetchProject = async () => {
+      try {
+        if (id) {
+          const response = await fetch(`/api/projects/${id}/projectUser`, {
+            method: "GET",
+            cache: "no-store",
             headers: { "Content-Type": "application/json" },
             next: { revalidate: 1 },
-          })
-          if(response.ok){
-            const data= await response.json();
-            setProject(data)
-          }else{
+          });
+          if (response.ok) {
+            const data = await response.json();
+            setProject(data);
+          } else {
             console.error("Error fetching hackathon:", response.statusText);
           }
         }
-      }catch(error){
+      } catch (error) {
         console.error("Error fetching the data:", error);
       }
+    };
+    fetchProject();
+  }, [id]);
+
+  useEffect(() => {
+    if (formattedCurrentDate > hackathon.endDate) {
+      router.push("/");
     }
-    fetchProject()
-  },[id])
+  }, [formattedCurrentDate, hackathon.endDate]);
 
   return (
     <Layout>
-      <div className="py-4 sm:py-12">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <ButtonSecondary buttonText={"Back"} buttonLink={`/hackathons/${id}`} />
-          <PageHeader
-            headerText={"Submit Project for " + hackathon.title }
-            descriptionText={"Fill out the form to complete the hackathon! :)"}
-          />
-        
-          {loading ? (
-            <p>Loading...</p>
-          ) : (
-            <ProjectSubmissionForm hackathonId={id} project={project} />
-          )}
+      {formattedCurrentDate <= hackathon.endDate && (
+        <div className="py-4 sm:py-12">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <ButtonSecondary
+              buttonText={"Back"}
+              buttonLink={`/hackathons/${id}`}
+            />
+            <PageHeader
+              headerText={"Submit Project for " + hackathon.title}
+              descriptionText={
+                "Fill out the form to complete the hackathon! :)"
+              }
+            />
+
+            {loading ? (
+              <p>Loading...</p>
+            ) : (
+              <ProjectSubmissionForm hackathonId={id} project={project} />
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </Layout>
   );
 }
