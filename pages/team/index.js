40c75518f1d0b61205e6d9518c5990ef45a5641b
Edgar Saravia <edgar.saravia@zebedee.io>
2024-03-13 09:50:00 -0600

    fixing the page teams for make it better responsive

--- pages/team/index.js
+++ pages/team/index.js
@@ -99,7 +99,7 @@ export default function TeamsPage({ session }) {
             />
           </div>
 
-          <div className=" grid grid-cols-1 md:grid-cols-3 gap-8 h-full">
+          <div className=" grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-8 h-full">
             {teams.map((team) => {
               return (
                 <>
