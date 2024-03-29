3cd4dbb7a4f0a4c41de17a770c371e3287aa8e96
Santos Hernandez <santos@zbd.gg>
2024-03-28 22:16:41 -0700

    fix.

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -13,8 +13,12 @@ export default async function handle(req, res) {
       return res.status(401).json({ error: 'Unauthorized' });
     }
 
-    const user = await prisma.user.findMany({});
-
+    const users = await prisma.user.findMany({
+      select: {
+        gamertag: true,
+        lightningAddress: true,
+      },
+    });
     res.json(user);
   } catch (error) {
     console.log(error);
